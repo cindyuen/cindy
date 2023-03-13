6516bf9cb8d64bf348b849ebbda432fb33376621
Cindy Yuen <77539036+cindyuen@users.noreply.github.com>
2023-03-13 18:52:43 +0000

    add hover on my icons and links

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import { BsFillMoonStarsFill } from "react-icons/bs";
 import { BsFillSunFill } from "react-icons/bs";
+import About from "../components/About";
 
 import {
   AiFillInstagram,
@@ -10,21 +11,10 @@ import {
   AiFillGithub,
   AiFillMail,
 } from "react-icons/ai";
-import Image from "next/image";
-import deved from "../public/undraw.svg";
-import design from "../public/design.png";
-import code from "../public/code.png";
-import consulting from "../public/consulting.png";
-import web1 from "../public/web1.png";
-import web2 from "../public/web2.png";
-import web3 from "../public/web3.png";
-import web4 from "../public/web4.png";
-import web5 from "../public/web5.png";
-import web6 from "../public/web6.png";
 import { useState } from "react";
 
 export default function Home(this: any) {
-  const [darkMode, setDarkMode] = useState(false);
+  const [darkMode, setDarkMode] = useState(true);
   return (
     <div className={darkMode ? "dark" : ""}>
       <Head>
@@ -57,7 +47,7 @@ export default function Home(this: any) {
               
               <a
                 className=" font-mono text-lg text-black font-bold px-4 py-2 rounded-md ml-8 dark:text-white"
-                href="https://drive.google.com/file/d/12zWu-43a81q7D52c09xo6WSIr8jvQ26-/view?usp=share_link"
+                href="http://localhost:3000/about-me"
                 target="_blank"
               >
                 {" "}
@@ -66,7 +56,7 @@ export default function Home(this: any) {
             </li>
             <li>
                <a
-                className=" text-center font-mono  text-lg  font-bold bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8"
+                className=" text-center font-mono  text-lg  font-bold  bg-gradient-to-r from-teal-300 to-purple-300 text-white  px-4 py-2 rounded-md ml-8"
                 href="https://drive.google.com/file/d/12zWu-43a81q7D52c09xo6WSIr8jvQ26-/view?usp=share_link"
                 target="_blank"
               >
@@ -77,35 +67,36 @@ export default function Home(this: any) {
           </ul>
         </nav>
         <section className="my-44">
-          <div className="text-left ">
-            <h2 className=" text-left  text-teal-400 font-bold md:text-5xl">
+          <div className="text-left font-mono ">
+            <h2 className=" text-left  text-teal-400 font-bold md:text-5xl  hover:text-purple-300">
               {" "}
-              Cindy Yuen
+              CINDY YUEN
             </h2>
-            <h3 className=" font-medium font-mono text-left py-6 md:text-xl dark:text-white">
+            <h3 className=" font-medium font-sans text-left py-6 md:text-xl dark:text-white">
               {" "}
-              I’m a UXUIX designer thrilled to provide better experience for people.
+              I’m a UXUX designer thrilled to provide better experience for people.
             </h3>
-            <p className="text-left py-2 md:text-lg font-mono  dark:text-zinc-200">
+            <p className="text-left py-2 md:text-lg font-sans  dark:text-zinc-200">
               I have a passion for design thinking, user research, user
               interface design, typography and use experience storytelling. I hope everyone has good experience on using a product.
             </p>
           </div>
           <div className="text-4xl flex text-left gap-8 py-6 text-gray-500">
-            <AiFillMail />
-            <AiFillLinkedin />
-            <AiFillGithub />
-            <AiFillDribbbleCircle/>
+            <a className="AiFillMail  hover:text-white" href="mailto:cindyyuen1011@gmail.com"> <AiFillMail /> </a>
+            <a className="AiFillMail  hover:text-white"  href="https://www.linkedin.com/in/cindy-yuen/">    <AiFillLinkedin /> </a>
+            <a className="AiFillMail hover:text-white"  href="https://github.com/cindyuen">    <AiFillGithub /> </a>
+            <a className="AiFillMail hover:text-white"  href="https://dribbble.com/cindyyuen">        
+            <AiFillDribbbleCircle/> </a>
+       
           </div>
       
         <div>
-          <h3  className="text-xl text-left font-bold font-mono py-28 dark: text-gray-500 underline ">
+          <h3  className="text-xl text-left font-bold font-mono pt-28 dark: text-gray-500 underline ">
             PORTFOLIO
           </h3>
-      
+
           <div className="py-2 grid grid-cols-2 md:grid-cols-2 gap-6  ">
             <div className="flex flex-col text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white">
-             
               <iframe
                 allowFullScreen
                 className="fp-iframe white w-full h-96"
@@ -141,8 +132,11 @@ export default function Home(this: any) {
             </div>
 </div>
 </div>
+
+      
         </section>
       </main>
     </div>
+    
   );
 }
